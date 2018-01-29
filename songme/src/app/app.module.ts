60d91f193123b848fdc7c9cf80f85b97653e820b
Zachary Biernat <zackbiernat@gmail.com>
2018-01-29 15:35:24 -0800

    Initializes app

---
+++ songme/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { GeneratorComponent } from './generator/generator.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GeneratorComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
