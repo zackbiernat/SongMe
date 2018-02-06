45cf00ff54e6c5df8bafe0c0f510ba3959fa9683
Zachary Biernat <zackbiernat@gmail.com>
2018-02-06 14:08:01 -0800

    Adds unique constraint to User schema

--- src/server/db/User.js
+++ src/server/db/User.js
@@ -4,6 +4,48 @@ const url = process.env.dbUrl;
 
 exports.addNewUser = (req, res) => {
   MongoClient.connect(url, (err, db) => {
-    res.send({message: 'You connected and responded!'});
+    checkIfUserExists(db, req.body.username, (notFound) => {
+      console.log('NF', notFound)
+      if (notFound) {
+        dbAddUser(db, req.body, (success) => {
+          if (success) {
+            res.send(success.ops[0]);
+          } else {
+            res.sendStatus(404);
+          }
+          db.close();
+        })
+      } else {
+        res.sendStatus(409); // Username already exists
+      }
+    });
+  })
+}
+
+//Signs up user
+const dbAddUser = (db, profile, cb) => {
+  let collection = db.collection('users');
+  collection.insert(profile, (err, success) => {
+    cb(success);
+  });
+
+}
+
+const checkIfUserExists = (db, username, cb) => {
+  let collection = db.collection('users');
+  collection.find({username: username})
+  .toArray((err, found) => {
+    if (found.length) {
+      cb(false);
+    } else {
+      cb(true);
+    }
+  })
+}
+
+
+exports.authenticateUser = (req, res) => {
+  MongoClient.connect(url, (err, db) => {
+
   })
 }
